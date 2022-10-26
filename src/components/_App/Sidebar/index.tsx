b4d1a0e23fb037775c27921bb079fb145c993965
bedirhandogan <bedirhandgn14@gmail.com>
2022-10-26 16:47:57 +0300

    refector: some updates

--- src/components/_App/Sidebar/index.tsx
+++ src/components/_App/Sidebar/index.tsx
@@ -1,22 +1,25 @@
 import { ArrowSmallLeftIcon } from '@heroicons/react/24/solid';
+import { CodeBracketSquareIcon } from '@heroicons/react/24/outline';
 import React from 'react';
 import './styles.css';
 
 interface SidebarProps {
     title: string;
-    homeButton: boolean;
-    children: React.ReactNode
+    sidebarType: string;
+    width: string,
+    children: React.ReactNode,
 }
 
-const Sidebar: React.FC<SidebarProps> = ({title, homeButton, children}) => {
+const Sidebar: React.FC<SidebarProps> = ({title, sidebarType, width, children}) => {
     return (
-        <div className={'sidebar container__limit'}>
+        <div className={`sidebar container__limit ${width}`}>
             <div className={'up-area'}>
-                { homeButton && <div className='relative tooltip flex items-center'>
+                { sidebarType === 'components' && <div className='relative tooltip flex items-center'>
                     <div className='tip'> Home </div>
                     <ArrowSmallLeftIcon className="h-5 w-5 text-fourth hover:text-[#7e7e7e] cursor-pointer duration-100 ease-linear"/>
                 </div>}
                 <h3 className="text-third">{title}</h3>
+                { sidebarType === 'code' && <CodeBracketSquareIcon className="h-5 w-5 text-fourth absolute right-3"/> }
             </div>
             {children}
         </div>
--- src/views/public/App.tsx
+++ src/views/public/App.tsx
@@ -1,27 +1,45 @@
-import Main from "components/Layout/Main";
-import Navbar from "components/Layout/Navbar";
-import Sidebar from "components/Layout/Sidebar";
-import './styles.css'
+import Main from "components/_App/Main";
+import Navbar from "components/_App/Navbar";
+import Sidebar from "components/_App/Sidebar";
+import "./styles.css";
 import { ExclamationTriangleIcon } from "@heroicons/react/24/solid";
+import { CopyBlock, dracula } from "react-code-blocks";
 
 export default function App() {
-    const screen = window.screen.width;
-    return (
-        <div className={`app bg-hideout-effect [&>#warning-information]:flex ${screen >= 1024 ? '[&>#warning-information]:hidden' : '[&>#warning-information]:block'}`}>
-            <div id="warning-information">
-                <ExclamationTriangleIcon className="w-16 sm:w-24 text-[#ffffff42]" />
-                <h2 className="text-fourth">
-                    You do not have enough screen size to view this page, please try again from the computer.
-                </h2>
-            </div>
-            <Sidebar title="Components" homeButton={true}>
-            </Sidebar>
-            <div className="w-2/6 flex flex-col items-center">
-                <Navbar />
-                <Main />
-            </div>
-            <Sidebar title="" homeButton={false}>
-            </Sidebar>
-        </div>
-    )
+  const screen = window.screen.width;
+
+  return (
+    <div
+      className={`app bg-circle-effect [&>#warning-information]:flex ${
+        screen >= 1024
+          ? "[&>#warning-information]:hidden"
+          : "[&>#warning-information]:block"
+      }`}
+    >
+      <div id="warning-information">
+        <ExclamationTriangleIcon className="w-16 sm:w-24 text-[#ffffff42]" />
+        <h2 className="text-fourth">
+          You do not have enough screen size to view this page, please try again
+          from the computer.
+        </h2>
+      </div>
+      <Sidebar title="Components" width="w-64" sidebarType="components">
+
+      </Sidebar>
+      <div className="w-2/6 flex flex-col items-center">
+        <Navbar />
+        <Main />
+      </div>
+      <Sidebar title="Code" width="w-96" sidebarType="code">
+        {/* <div className="copy-block">
+          <CopyBlock
+            text={code}
+            language={"css"}
+            theme={dracula}
+            codeBlock
+          />
+        </div> */}
+      </Sidebar>
+    </div>
+  );
 }
